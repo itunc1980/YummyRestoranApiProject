0857b767c19f67f0fcd550b8406b6a97a5a77e8f
Burak <Burak_ozyurt42@outlook.com>
2026-02-22 14:01:42 +0300

    navbar bittiiii

---
+++ ApiProject.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarProfileMenuAdminLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.ViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _NavbarProfileMenuAdminLayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
