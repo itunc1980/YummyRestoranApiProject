2c3a70363c67c467049cb22a27f911e88abb08f2
Burak <Burak_ozyurt42@outlook.com>
2026-03-04 14:41:06 +0300

    arayüz yapılıyor

---
+++ ApiProject.WebUI/ViewComponents/DashboardViewComponents/_DashboardEmployeeTaskComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardEmployeeTaskComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardEmployeeTaskComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
