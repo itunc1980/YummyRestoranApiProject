4a66627c7fcc597ddd8bb109a0366640369c65c4
Burak <Burak_ozyurt42@outlook.com>
2026-03-06 12:53:47 +0300

    galeri bittii

--- ApiProject.WebUI/Controllers/GalleryController.cs
+++ ApiProject.WebUI/Controllers/GalleryController.cs
@@ -1,4 +1,5 @@
-﻿using ApiProject.WebUI.Dtos.CategoryDtos;
+﻿using System.Text;
+using ApiProject.WebUI.Dtos.ImageDtos;
 using ApiProject.WebUI.Dtos.ImageDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -24,6 +25,66 @@ namespace ApiProject.WebUI.Controllers
                 return View(values);
             }
             return View();
+        }
+
+        public async Task<IActionResult> ImageListWithEdit()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7123/api/Images");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultImageDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateImage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateImage(CreateImageDto createImageDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createImageDto);  //metinden jsona =======> deserialize ise jsondan metine
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7123/api/Images", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ImageListWithEdit");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteImage(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync("https://localhost:7123/api/Images?id=" + id);
+            return RedirectToAction("ImageListWithEdit");
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> UpdateImage(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7123/api/Images/GetImage?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<GetImageByIdDto>(jsonData);
+            return View(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateImage(UpdateImageDto updateImageDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateImageDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7123/api/Images", stringContent);
+            return RedirectToAction("ImageListWithEdit");
         }
     }
 }
