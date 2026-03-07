125e6a0599f42956f5d9806e8840b3e14f212935
Burak <Burak_ozyurt42@outlook.com>
2026-03-07 13:11:28 +0300

    dashboard az kaldıı

---
+++ ApiProject.WebUI/ViewComponents/DashboardViewComponents/_DashboardAIDailyMenuSuggestionComponentPartial.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardAIDailyMenuSuggestionComponentPartial:ViewComponent
+    {
+    }
+}
